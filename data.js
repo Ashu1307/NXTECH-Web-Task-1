a0bd7df9e159ae094755bccf88d9b60757fe37a2
Ashu1307 <141839249+Ashu1307@users.noreply.github.com>
2023-08-09 19:28:01 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,35 @@
+
+let songs = [
+        {
+            name: 'Vele',
+            path: 'Song 2.mp3',
+            artist: 'Shekhar Ravjiani , vishal dadlani, vishal shekhar',
+            cover: 'cover 2.png'
+            
+        },
+        {
+            name: ' tera yarr hun mein ',
+            path: 'Song 1.mp3',
+            artist: 'Arjit singh',
+            cover: 'cover 1.jpg',
+        },
+        {
+            name: 'Kesariya',
+            path: 'Song 3.mp3',
+            artist: 'Arjit singh',
+            cover: 'cover 3.jpg'
+        },
+        {
+            name: 'veere di wedding ',
+            path: 'Song 4.mp3',
+            artist: 'mika singh',
+            cover: 'cover 4.jpg'
+        
+        },
+        {
+            name: 'buddhu sa man',
+            path: 'Song 5.mp3',
+            artist: 'amal malik , araman malik',
+            cover: 'cover 5.png'
+        }
+           ]
